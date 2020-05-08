0594a6d41e826ba7358a398e8db425691270090f
FedorovVladimir <vladimir.fodorow@gmail.com>
2020-05-09 04:14:55 +0700

    update models

--- groups/models.py
+++ groups/models.py
@@ -6,3 +6,9 @@ from subjects.models import Subject
 class Group(models.Model):
     name = models.CharField(max_length=30)
     subject = models.ManyToManyField(Subject)
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
--- subjects/models.py
+++ subjects/models.py
@@ -3,3 +3,18 @@ from django.db import models
 
 class Subject(models.Model):
     name = models.CharField(max_length=30)
+    level_3 = models.FloatField(default=25)
+    level_4 = models.FloatField(default=50)
+    level_5 = models.FloatField(default=75)
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=30)
+    deadline = models.DateField()
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=30)
+    date = models.DateField()
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
