46a265dd4984aac328d36b5b10b76d3de1f56dbf
FedorovVladimir <vladimir.fodorow@gmail.com>
2020-05-09 00:32:42 +0700

    add wrapper and templates

--- groups/views.py
+++ groups/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("<h3>Hello, groups!</h3>")
+    return render(request, "groups/index.html")
